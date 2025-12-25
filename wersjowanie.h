7aaba082eb6c4ec04798f484c5fd42a0e73e27d9
karolgotow <karol.gotowala@proton.me>
2025-12-25 15:41:44 +0000

    Add files via upload

---
+++ wersjowanie.h
@@ -0,0 +1,5 @@
+#define WERSJA CLIWERSJA//-DCLIWERSJA=1 dla gcc 11 aby przypisywac makra z poziomu cli kompilatora -> gcc sterta.c -o stertac -DCLIWERSJA=1
+		//^albo recznie zamienic CLIWERSJA w tym pliku 
+#define STOSTAB 1
+#define STERTAWSK 2 //tu powinna byc sterta ?
+#define STERTARETURN 3
